0031125061e26b5f87d86440cb77eb537c7f9469
27bslash <greenpear68@yahoo.co.uk>
2022-04-12 13:59:57 +0100

    fixed overlay deleting list from storage

--- src/components/content/overlay.tsx
+++ src/components/content/overlay.tsx
@@ -1,10 +1,11 @@
 import { SetStateAction, useState } from 'react';
 import Manga from './../../types/manga';
 import { useEffect } from 'react';
-
+interface Test {
+    any: string
+}
 const Overlay = () => {
     const data = extractTitle(document.title)
-    console.log('data', data)
     // listenForMessages();
     // console.log('app')
     const [showPrompt, setShowPrompt] = useState(true)
@@ -12,20 +13,38 @@ const Overlay = () => {
     chrome.storage.sync.get("id", (result) => {
         setId(result.id)
     });
+    const getLatest = (source: {
+        [x: string]: {
+            url: string;
+            latest: string;
+            latest_link: string;
+        };
+    }, key: string, chapter: string) => {
+        if (source[key]) {
+            if ('latest' in source[key]) {
+                return { 'url': data['link'], 'latest': source[key].latest, 'latest_link': source[key].latest_link }
+            }
+        }
+        return { 'url': data['link'], 'latest': chapter, 'latest_link': data['link'] }
+    }
+
     useEffect(() => {
         chrome.storage.sync.get('manga-list', (result) => {
             result['manga-list'].forEach((x: Manga) => {
                 if (x['title'] === data['title']) {
                     // update chapter
-                    x['link'] = window.location.href
-                    x['domain'] = window.location.origin
-                    x['read'] = false
-                    x['chapter'] = data['chapter']
-                    if (x['latest'] === data['chapter']) {
-                        x['read'] = true
-                    }
                     setShowPrompt(false)
-                    addNewManga(x, updatePrompt)
+                    if (data['chapter'] > x['chapter']) {
+                        x['chapter'] = data['chapter']
+                        x['link'] = data['link']
+                        if (!('sources' in x)) {
+                            x['sources'] = {}
+                        }
+                        x['sources'][data['scansite']] = getLatest(x['sources'], data['scansite'], data['chapter'])
+                        x['sources']['any'] = x['sources'][data['scansite']]
+                        console.log('updated series info:', x)
+                        addNewManga(x, updatePrompt)
+                    }
                 }
             })
         })
@@ -36,7 +55,7 @@ const Overlay = () => {
     }
     return (
         +data['chapter'] > 10 && showPrompt ? (
-            <div className='manga-overlay'>{data['title']}, {data['chapter']},{id} start watching?
+            <div className='manga-overlay'>start reading ?
                 <p onClick={() => addNewManga(data, updatePrompt)}>ok</p>
                 <p onClick={() => setShowPrompt(false)} >cancel</p>
             </div >
@@ -45,21 +64,42 @@ const Overlay = () => {
         )
     )
 }
-const addNewManga = (data: object, updatePrompt: Function) => {
+const addNewManga = (data: any, updatePrompt: Function) => {
     // console.log(url)
     chrome.runtime.sendMessage({ type: 'update', data: data }, (response) => {
         console.log('add new', response)
+
+    })
+    chrome.storage.sync.get('manga-list', (result) => {
+        const list = result['manga-list']
+        list.forEach((element: Manga) => {
+            if (element.title === data.title) {
+                element = data
+            }
+        });
+        chrome.storage.sync.set({ 'manga-list': list })
     })
     updatePrompt(false)
 }
 const extractTitle = (title: string) => {
     let chapterNum = "",
-        seriesTitle = "",
         scanSite = "";
-
+    const scanRegex = /\w+\s?\w+$/gim;
+    const chapterRegex = /(?<=episode\s|chapter\s|#)\d+\.?\d*/im;
+    const cleanTitle = (title: string) => {
+        let seriesTitle = title.replace(scanRegex, '').trim();
+        seriesTitle = seriesTitle
+            // remove chapter numbers
+            .replace(chapterRegex, "")
+            .replace(/chapter|episode/gi, "")
+            // remove special characters
+            .replace(/:|-|-|\||\[#\]/gm, "")
+            .trim()
+            .replace(/\s/g, '-')
+            .toLowerCase()
+        return seriesTitle
+    }
     const scans = (title: string) => {
-        const scanRegex = /\w+\s?\w+$/gim;
-        seriesTitle = title.replace(scanRegex, "");
         if (title.toLowerCase().includes("episode")) {
             // webttons edge case
             scanSite = "webtoons";
@@ -71,30 +111,22 @@ const extractTitle = (title: string) => {
         }
     };
     const getChapterNum = (title: string) => {
-        const chapterRegex = /(?<=episode\s|chapter\s|#)\d+\.?\d*/im;
         // get number after chapter then remove leading zeros
         const match = title.match(chapterRegex);
         if (match) {
-            console.log('match', match)
             chapterNum = match[0]
         }
-        seriesTitle = seriesTitle
-            // remove chapter numbers
-            .replace(chapterRegex, "")
-            .replace(/chapter|episode/gi, "")
-            // remove special characters
-            .replace(/:|-|-|\||\[#\]/gm, "")
-            .trim()
-            .replace(/\s/g, '-')
-            .toLowerCase()
+
         if (chapterNum) {
             chapterNum = chapterNum !== '0' ? chapterNum.replace(/^0+/, "") : '0';
         }
     };
     scans(title);
     getChapterNum(title);
-    return { title: seriesTitle, chapter: chapterNum, scansite: scanSite };
+    const seriesTitle = cleanTitle(title);
+    return { title: seriesTitle, chapter: chapterNum, scansite: scanSite, domain: window.location.origin, link: window.location.href, sources: [{ scanSite: window.location.href }] }
 };
+
 const listenForMessages = () => {
     console.log('listen messga')
     chrome.runtime.onMessage.addListener((message, sender) => {
@@ -104,5 +136,5 @@ const listenForMessages = () => {
     });
 };
 export default Overlay
-// I made my own version of dota2protracker:
-// https://dota2-item-tracker.herokuapp.com/
+
+
