c5afbda85dba1b6866597020b5ad4b7e0f215038
27bslash <greenpear68@yahoo.co.uk>
2022-04-14 15:52:53 +0100

    basic global theme

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -1,10 +1,28 @@
 import ReactDOM from "react-dom";
-import CheckboxList from "./manga-list";
+import CheckboxList from "./manga-list/manga-list";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 function Popup() {
   return (
-    <div className="Popup">
-      <CheckboxList/>
-    </div>
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          type: "dark",
+          primary: {
+            main: "#26a27b",
+          },
+          secondary: {
+            main: "#fafafa",
+          },
+        },
+      })}
+    >
+      <CssBaseline />
+      <div className="Popup">
+        <CheckboxList />
+      </div>
+    </ThemeProvider>
   );
 }
 export default Popup;
