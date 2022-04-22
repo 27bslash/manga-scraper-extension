955184dacfaca3b9a798a99508bd2bd176a3a264
27bslash <greenpear68@yahoo.co.uk>
2022-04-22 16:58:08 +0100

    changed colors of nav bar

--- src/components/popup/nav/list-top.tsx
+++ src/components/popup/nav/list-top.tsx
@@ -5,7 +5,7 @@ import TabToggler from './tab-toggler';
 
 export default function BasicTabs(props: any) {
     return (
-        <Box sx={{ width: '100%', borderBottom: 1, borderColor: 'divider', bgColor: 'primary.dark' }}>
+        <Box className='test-box' sx={{ width: '100%', borderBottom: 1, borderColor: 'divider', backgroundColor: 'rgb(30, 41, 101)' }}>
             <TabToggler {...props} />
         </Box >
     );
