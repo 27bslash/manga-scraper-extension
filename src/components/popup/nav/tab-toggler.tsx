dd2c6f31c48b5c89fa44eb2bf2ddf175f5a507a5
27bslash <greenpear68@yahoo.co.uk>
2022-04-22 16:57:44 +0100

    nav tabs don't show if nothing in tab

--- src/components/popup/nav/tab-toggler.tsx
+++ src/components/popup/nav/tab-toggler.tsx
@@ -1,6 +1,7 @@
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import * as React from 'react';
+import Manga from '../../../types/manga';
 
 interface TabtogglerProps {
     showAll: boolean,
@@ -9,32 +10,43 @@ interface TabtogglerProps {
     handleDelete: (value?: number) => void,
     updateEditing: (value: boolean) => void,
     updateRead: (b: boolean) => void,
+    totalData: Manga[],
 }
 function TabToggler(props: TabtogglerProps) {
     const [value, setValue] = React.useState(0);
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
+    const filteredData = props.totalData.filter((x: Manga) => !x.read)
     return (
         <>
-            {props.checked.length === 0 && (
-                <Tabs value={value} onChange={handleChange} centered aria-label="basic tabs example" TabIndicatorProps={{
+            {props.checked.length === 0 && filteredData.length > 0 && (
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" TabIndicatorProps={{
                     style: {
-                        background: "red",
+                        background: 'rgb(119, 119, 203)',
                     },
                 }} >
-                    <Tab className='align-right' label="Unread" onClick={() => props.handleClick(false, 'toggleView')} />
-                    <Tab label="All series" onClick={() => props.handleClick(true, 'toggleView')} />
+                    <Tab sx={{ color: '#fff !important' }} className='align-right' label="Unread" onClick={() => props.handleClick(false, 'toggleView')} />
+                    <Tab sx={{ color: '#fff !important' }} label="All series" onClick={() => props.handleClick(true, 'toggleView')} />
                 </Tabs>
             )}
+            {props.checked.length === 0 && filteredData.length === 0 && (
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" TabIndicatorProps={{
+                    style: {
+                        background: "rgb(119, 119, 203)",
+                    }
+                }}>
+                    <Tab sx={{ color: '#fff !important' }} label="All series" onClick={() => props.handleClick(true, 'toggleView')} />
+                </Tabs>
+            )}
+
             {props.checked.length > 0 && props.showAll && (
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" TabIndicatorProps={{
                     style: {
                         display: 'none',
                     },
                 }} >
-                    <Tab label="Delete" onClick={() => props.handleDelete()} />
-                    <Tab className='align-right' label="un-read" onClick={() => props.updateRead(false)} />
+                    <Tab sx={{ color: '#fff !important' }} label="Delete" onClick={() => props.handleDelete(-1)} />
                 </Tabs>
 
             )
@@ -45,7 +57,7 @@ function TabToggler(props: TabtogglerProps) {
                         display: 'none',
                     },
                 }} >
-                    <Tab className='align-right' label="read" onClick={() => props.updateRead(true)} />
+                    <Tab sx={{ color: '#fff !important' }} className='align-right' label="read" onClick={() => props.updateRead(true)} />
                 </Tabs>
 
             )
