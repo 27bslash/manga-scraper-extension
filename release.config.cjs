1f5fa2a3b0facba98b117f11af3beef3a3c0378a
27bslash <greenpear68@yahoo.co.uk>
2026-06-02 18:54:45 +0100

    chore: updated release script version name

--- release.config.cjs
+++ release.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   branches: ["master"],
-  tagFormat: "firefox-v${version}",
+  tagFormat: "v${version}",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
